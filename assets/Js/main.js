274d6cd91fc837a1082115d4d99152795f93fe0a
L-cir94 <luca.cirigliano94@hotmail.it>
2023-03-24 14:57:45 +0100

    markup

--- assets/Js/main.js
+++ assets/Js/main.js
@@ -1,12 +1,31 @@
 
 /* consegna
+Dato un array di oggetti letterali con:
+- url dell’immagine
+- titolo
+- descrizione
+Creare un carosello come nella foto allegata.
 
+Come nel primo carosello realizzato, focalizziamoci prima sulla creazione del 
+markup statico: costruiamo il container e inseriamo l'immagine grande in modo da 
+poter stilare lo slider.
+
+- Ora rimuoviamo i contenuti statici e usiamo l’array di oggetti letterali per popolare 
+dinamicamente il carosello.
+
+- Al click dell'utente sulle frecce verso sinistra o destra, l'immagine attiva diventerà visibile e dovremo aggiungervi titolo e testo.
+- Aggiungere il ciclo infinito del carosello. Ovvero se la miniatura attiva è la prima e l'utente clicca la freccia verso destra, la miniatura che deve attivarsi sarà l'ultima e viceversa per l'ultima miniatura se l'utente clicca la freccia verso sinistra.
+
+bonus
+- Aggiungere le thumbnails (sottoforma di miniatura) ed al click attivare l’immagine corrispondente.
+- Aggiungere funzionalità di autoplay: dopo un certo periodo di tempo (3 secondi) l’immagine attiva dovrà cambiare alla successiva.
+- Aggiungere bottoni di start/stop e di inversione del meccanismo di autoplay.
 */
 
 /* strumenti: 
 
 */
-const imgList= [
+const images = [
     {
         image: 'img/01.webp',
         title: 'Marvel\'s Spiderman Miles Morale',
@@ -29,5 +48,26 @@ const imgList= [
         text: 'Marvel\'s Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay.',
     }
 ];
-const carouselElement = document.querySelector(".carousel");
 
+//mi seleziono gli elementi della DOM
+let active_slide = 0
+const slides_dom_element = document.querySelector('.slides')
+const thumbs_dom_element = document.querySelector('.thumbs')
+const prev_dom_element = document.querySelector('button.prev')
+const next_dom_element = document.querySelector('button.next')
+const slider_dom_element = document.querySelector('.slider')
+const revet_dom_elemet = document.querySelector('button.revert')
+
+//Ora rimuoviamo i contenuti statici e usiamo l’array di oggetti letterali per popolare dinamicamente il carosello.
+images.forEach((slide, index) => {
+    const activeSlide = index === active_slide ? 'active' : ''
+    const slide_markup = `
+    <div class="slide ${activeSlide}">
+      <img src="./assets/${slide.image}" alt="">
+      <div class="text">
+        <h3>${slide.title}</h3>
+        <p>${slide.text}</p>
+      </div>
+    </div>
+    `
+})
